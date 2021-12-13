8bd38d78e9d65e246b223ee6120cb77819458bf8
0Roni0 <84076559+0Roni0@users.noreply.github.com>
2021-12-13 13:37:54 +0300

    Add files via upload

---
+++ zadanie/idz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Вариант 12
+
+
+if __name__ == "__main__":
+    U = set("abcdefghijklmnopqrstuvwxyz")
+    A = {'b', 'k', 'n', 'o', 'r'}
+    B = {'a', 'b', 'k', 'u'}
+    C = {'o', 'p'}
+    D = {'a', 'm', 'n', 'y', 'z'}
+
+    X = (A.union(B)).intersection(D)
+    print(f'X= {X}')
+
+    AA = U.difference(A)
+
+    Y = (AA.intersection(D)).union(C.difference(B))
+    print(f'Y = {Y}')
